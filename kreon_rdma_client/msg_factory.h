d46e70a04955934ec5e829df1b755542a6be01e1
Giorgos Stylianakis <geostyl@ics.forth.gr>
2022-11-10 16:56:51 +0200

    [refactor] Add externs to header files

--- kreon_rdma_client/msg_factory.h
+++ kreon_rdma_client/msg_factory.h
@@ -10,29 +10,29 @@ struct put_msg_data {
 	char *value;
 };
 
-int32_t calculate_put_msg_size(int32_t key_size, int32_t value_size);
+extern int32_t calculate_put_msg_size(int32_t key_size, int32_t value_size);
 
-int32_t calculate_get_msg_size(int32_t key_size);
+extern int32_t calculate_get_msg_size(int32_t key_size);
 
-void create_put_msg(struct put_msg_data data, char *msg);
+extern void create_put_msg(struct put_msg_data data, msg_header *msg_header);
 
-struct kv_splice *put_msg_get_kv_offset(msg_header *msg);
+extern struct kv_splice *put_msg_get_kv_offset(msg_header *msg);
 
-struct lsn *put_msg_get_lsn_offset(msg_header *msg);
+extern struct lsn *put_msg_get_lsn_offset(msg_header *msg);
 
-int32_t put_msg_get_payload_size(msg_header *msg);
+extern int32_t put_msg_get_payload_size(msg_header *msg);
 
-void create_get_msg(int32_t key_size, char *key, int32_t reply_size, char *msg_payload_offt);
+extern void create_get_msg(int32_t key_size, char *key, int32_t reply_size, char *msg_payload_offt);
 
-int32_t get_msg_get_fetch_value(msg_header *msg);
+extern int32_t get_msg_get_fetch_value(msg_header *msg);
 
-int32_t get_msg_get_bytes_to_read(msg_header *msg);
+extern int32_t get_msg_get_bytes_to_read(msg_header *msg);
 
-int32_t get_msg_get_offset(msg_header *msg);
+extern int32_t get_msg_get_offset(msg_header *msg);
 
-int32_t get_msg_get_key_size(msg_header *msg);
+extern int32_t get_msg_get_key_size(msg_header *msg);
 
-char *get_msg_get_key_offset(msg_header *msg);
+extern char *get_msg_get_key_offset(msg_header *msg);
 
-void put_msg_print_msg(msg_header *msg);
+extern void put_msg_print_msg(msg_header *msg);
 #endif //MSG_FACTORY_H
--- kreon_server/globals.h
+++ kreon_server/globals.h
@@ -4,36 +4,37 @@
 #include <stdint.h>
 
 enum operation_type { TEB_PUT = 0, TEB_GET };
-char *globals_get_RDMA_IP_filter(void);
-void globals_set_RDMA_IP_filter(char *RDMA_IP_filter);
+extern char *globals_get_RDMA_IP_filter(void);
+extern void globals_set_RDMA_IP_filter(char *RDMA_IP_filter);
 
-char *globals_get_zk_host(void);
-void globals_set_zk_host(char *host);
+extern char *globals_get_zk_host(void);
+extern void globals_set_zk_host(char *host);
 
-int globals_get_connections_per_server(void);
+extern int globals_get_connections_per_server(void);
 
-int globals_get_job_scheduling_max_queue_depth(void);
+extern int globals_get_job_scheduling_max_queue_depth(void);
 
-int globals_get_worker_spin_time_usec(void);
+extern int globals_get_worker_spin_time_usec(void);
 
-void globals_set_dev(char *dev);
-char *globals_get_dev(void);
-uint64_t globals_get_dev_size(void);
+extern void globals_set_dev(char *dev);
+extern char *globals_get_dev(void);
+extern uint64_t globals_get_dev_size(void);
 
-void globals_create_rdma_channel(void);
-struct channel_rdma *globals_get_rdma_channel(void);
+extern void globals_create_rdma_channel(void);
+extern struct channel_rdma *globals_get_rdma_channel(void);
 
-void globals_init_volume(void);
+extern void globals_init_volume(void);
 
-void globals_set_l0_size(uint32_t l0_size);
-uint32_t globals_get_l0_size(void);
+extern void globals_set_l0_size(uint32_t l0_size);
+extern uint32_t globals_get_l0_size(void);
 
-void globals_set_growth_factor(uint32_t growth_factor);
-uint32_t globals_get_growth_factor(void);
+extern void globals_set_growth_factor(uint32_t growth_factor);
+extern uint32_t globals_get_growth_factor(void);
 
-void globals_set_send_index(int enable);
-int globals_get_send_index(void);
+extern void globals_set_send_index(int enable);
+extern int globals_get_send_index(void);
 
-void globals_open_trace_file(const char *filename);
-void globals_append_trace_file(uint32_t key_size, void *key, uint32_t value_size, void *value, enum operation_type op);
-void globals_close_trace_file(void);
+extern void globals_open_trace_file(const char *filename);
+extern void globals_append_trace_file(uint32_t key_size, void *key, uint32_t value_size, void *value,
+				      enum operation_type op);
+extern void globals_close_trace_file(void);
