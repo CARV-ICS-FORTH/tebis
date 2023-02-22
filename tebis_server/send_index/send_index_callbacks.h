8790ea7d66a3c4f3f8841e3eae2a8b156e1822a3
Giorgos Stylianakis <geostyl@ics.forth.gr>
2023-02-22 12:30:08 +0200

    [refactor] Remove compaction started cb from send_index.h

--- tebis_server/send_index/send_index_callbacks.h
+++ tebis_server/send_index/send_index_callbacks.h
@@ -23,10 +23,6 @@ struct send_index_context {
 	struct regs_server_desc *server;
 };
 
-void send_index_compaction_started_callback(void *context, uint64_t small_log_tail_dev_offt,
-					    uint64_t big_log_tail_dev_offt, uint32_t src_level_id, uint8_t dst_tree_id,
-					    struct wcursor_level_write_cursor *new_level);
-
 void send_index_init_callbacks(struct regs_server_desc *server, struct region_desc *r_desc);
 
-#endif // SEND_INDEX_CALLBACKS_H_
+#endif // SEND_INDEX_CALLBACKS_H
