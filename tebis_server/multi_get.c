f48d614b5557c892209050148b6d9a50d6c002eb
Giorgos Saloustros <gesalous@ics.forth.gr>
2023-10-06 15:09:30 +0300

    [fix] Calculate correctly repsonse size

--- tebis_server/multi_get.c
+++ tebis_server/multi_get.c
@@ -22,6 +22,7 @@ struct multi_get {
 
 static enum work_task_status mget_process(const struct request *request, struct work_task *task)
 {
+	// static uint64_t served_requests;
 	if (request->type != MULTI_GET_REQUEST) {
 		log_fatal("Wrong request type Cannot handle!");
 		_exit(EXIT_FAILURE);
@@ -41,22 +42,23 @@ static enum work_task_status mget_process(const struct request *request, struct 
 	struct msg_multi_get_rep *mget_reply =
 		(struct msg_multi_get_rep *)&((char *)task->reply_msg)[sizeof(msg_header)];
 
+	uint32_t max_KVs = mget->mget_msg->max_num_entries;
+	uint32_t num_KVs = 0;
+	uint32_t available_bytes = mget->req.net_request->reply_length_in_recv_buffer -
+				   (sizeof(msg_header) + sizeof(*mget_reply) + MESSAGE_SEGMENT_SIZE);
+	uint32_t response_size = 0;
+	char *KV_buffer = &((char *)mget_reply)[sizeof(*mget_reply)];
+
 	//Parallax start
 	if (!region_desc_enter_parallax(region, task)) {
 		// later...
 		return TASK_START;
 	}
 
-	uint32_t max_KVs = mget->mget_msg->max_num_entries;
-	uint32_t num_KVs = 0;
-	uint32_t available_bytes = mget->req.net_request->reply_length_in_recv_buffer - sizeof(*mget_reply);
-	uint32_t response_size = 0;
-	char *KV_buffer = &((char *)mget_reply)[sizeof(*mget_reply)];
-
 	const char *error = NULL;
 	struct par_key seek_key = { .size = mget->mget_msg->seek_key_size, .data = mget->mget_msg->seek_key };
 	par_scanner scanner = par_init_scanner(region_desc_get_db(region), &seek_key, PAR_GREATER_OR_EQUAL, &error);
-	log_info("Searching for up to max_KVS: %u", max_KVs);
+	// log_info("Searching for up to max_KVS: %u", max_KVs);
 	while (par_is_valid(scanner) && num_KVs < max_KVs) {
 		struct par_key key = par_get_key(scanner);
 		struct par_value value = par_get_value(scanner);
@@ -80,13 +82,16 @@ static enum work_task_status mget_process(const struct request *request, struct 
 	region_desc_leave_parallax(region);
 
 	//Parallax end
-	log_info("Retrieved %u keys total response size: %u", num_KVs, response_size);
-	msg_fill_reply_header(task->reply_msg, task->msg, response_size, MULTI_GET_REPLY);
+	// log_info("Retrieved %u KV pairs of %u requested total response size: %u", num_KVs, max_KVs, response_size);
+	msg_fill_reply_header(task->reply_msg, (struct msg_header *)mget->req.net_request,
+			      sizeof(msg_multi_get_rep) + response_size, MULTI_GET_REPLY);
 
 	mget_reply->num_entries = num_KVs;
 	mget_reply->curr_entry = 0;
 	mget_reply->end_of_region = 0;
 	msg_set_receive_field(task->reply_msg, TU_RDMA_REGULAR_MSG);
+	// log_info("Served %lu multi gets", ++served_requests);
+
 	return TASK_COMPLETE;
 }
 
