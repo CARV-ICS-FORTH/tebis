d3fd6c8397fa009e20ac44289aeb9b1f15bfd926
Giorgos Saloustros <gesalous@ics.forth.gr>
2023-10-09 17:26:17 +0300

    [feat] Deploy executable and monitoring subsystem

--- ssh_deploy/deploy_servers.sh
+++ ssh_deploy/deploy_servers.sh
@@ -1,24 +1,53 @@
 #!/bin/bash
 
-# Check if at least 3 arguments are provided
-if [ "$#" -lt 3 ]; then
-	echo "Usage: $0 <executable> <path> <server1> [<server2>...]"
+# Check if at least 2 arguments are provided
+if [ "$#" -lt 2 ]; then
+	echo "Usage: $0 <executable_path> <script_folder> [bastion_host] <server1> [<server2>...]"
 	exit 1
 fi
 
-EXECUTABLE="$1"
-DEST_PATH="$2"
-shift
-shift # Remove first two arguments to get only the list of servers
+# Parse the arguments
+EXECUTABLE_PATH="$1"
+SCRIPT_FOLDER="$2"
+shift 2 # Remove the first two arguments (executable_path and script_folder)
 
+# Check if a bastion host is provided as an optional argument
+if [ "$1" == "bastion_host" ]; then
+	if [ -z "$2" ]; then
+		echo "Error: Bastion host argument is empty."
+		exit 1
+	fi
+	BASTION_HOST="$2"
+	shift 2 # Remove bastion_host argument and its value
+fi
+
+echo "Bastion host = $BASTION_HOST"
 # Iterate over all server arguments
 for SERVER in "$@"; do
-	echo "Transferring $EXECUTABLE to $SERVER:$DEST_PATH ..."
-	scp "$EXECUTABLE" "$SERVER:$DEST_PATH"
-	if [ $? -eq 0 ]; then
-		echo "Successfully transferred to $SERVER."
+	# Transfer the executable to the server
+	if [ -n "$BASTION_HOST" ]; then
+		scp -o ProxyJump="$BASTION_HOST" "$EXECUTABLE_PATH" "$SERVER:"
 	else
-		echo "Transfer to $SERVER failed."
+		scp "$EXECUTABLE_PATH" "$SERVER:"
+	fi
+
+	if [ $? -eq 0 ]; then
+		echo "Successfully transferred $EXECUTABLE_PATH to $SERVER."
+	else
+		echo "Transfer of $EXECUTABLE_PATH to $SERVER failed."
+	fi
+
+	# Transfer all .sh scripts from the specified folder to /tmp on the remote machine
+	if [ -n "$BASTION_HOST" ]; then
+		scp -o ProxyJump="$BASTION_HOST" "$SCRIPT_FOLDER"/*.sh "$SERVER:/tmp"
+	else
+		scp "$SCRIPT_FOLDER"/*.sh "$SERVER:/tmp"
+	fi
+
+	if [ $? -eq 0 ]; then
+		echo "Successfully transferred .sh scripts to $SERVER:/tmp."
+	else
+		echo "Transfer of .sh scripts to $SERVER:/tmp failed."
 	fi
 done
 
