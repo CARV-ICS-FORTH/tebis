08d903f3003d84efe80e90cafdf1b4aa7d00a92b
Dimokratis Stathakis <dstath@ics.forth.gr>
2024-07-24 18:05:18 +0300

    [feat] YCSB pod added on deploy and remove_sources scripts

--- dockers/deploy.sh
+++ dockers/deploy.sh
@@ -31,15 +31,27 @@ function deploy_tebis() {
 	fi
 }
 
+function deploy_ycsb() {
+	echo "YCSB deploy from Kubernetes..."
+	if sudo kubectl apply -f YCSB_CXX/ycsb-pod.yaml; then
+		echo "YCSB deploy complete."
+	else
+		echo "YCSB deploy failed."
+		exit 1
+	fi
+}
+
 # Flags to track which functions to call
 DO_deploy_ZOOKEEPER=false
 DO_deploy_script=false
 DO_deploy_TEBIS=false
+DO_deploy_YCSB=false
 
 # Process input arguments
 if [ "$#" -eq 0 ]; then
 	DO_deploy_ZOOKEEPER=true
 	DO_deploy_TEBIS=true
+	DO_deploy_YCSB=true
 else
 	for arg in "$@"; do
 		case $arg in
@@ -52,8 +64,11 @@ else
 		tebis)
 			DO_deploy_TEBIS=true
 			;;
+		ycsb)
+			DO_deploy_YCSB=true
+			;;
 		*)
-			echo "Invalid parameter: $arg. Use 'zoo', 'script', 'tebis', or no parameter for all."
+			echo "Invalid parameter: $arg. Use 'zoo', 'script', 'tebis', 'ycsb', or no parameter for all."
 			exit 1
 			;;
 		esac
@@ -71,4 +86,8 @@ fi
 
 if [ "$DO_deploy_TEBIS" = true ]; then
 	deploy_tebis
+fi
+
+if [ "$DO_deploy_YCSB" = true ]; then
+	deploy_ycsb
 fi
--- dockers/remove_sources.sh
+++ dockers/remove_sources.sh
@@ -21,14 +21,26 @@ function remove_tebis() {
 	fi
 }
 
+function remove_ycsb() {
+	echo "YCSB remove from Kubernetes..."
+	if sudo kubectl delete -f YCSB_CXX/ycsb-pod.yaml; then
+		echo "YCSB remove complete."
+	else
+		echo "YCSB remove failed."
+		exit 1
+	fi
+}
+
 # Flags to track which functions to call
 DO_REMOVE_ZOOKEEPER=false
 DO_REMOVE_TEBIS=false
+DO_REMOVE_YCSB=false
 
 # Process input arguments
 if [ "$#" -eq 0 ]; then
 	DO_REMOVE_ZOOKEEPER=true
 	DO_REMOVE_TEBIS=true
+	DO_REMOVE_YCSB=true
 else
 	for arg in "$@"; do
 		case $arg in
@@ -38,8 +50,11 @@ else
 		tebis)
 			DO_REMOVE_TEBIS=true
 			;;
+		ycsb)
+			DO_REMOVE_YCSB=true
+			;;
 		*)
-			echo "Invalid parameter: $arg. Use 'zoo', 'pv', 'tebis', or no parameter for all."
+			echo "Invalid parameter: $arg. Use 'zoo', 'tebis', 'ycsb', or no parameter for all."
 			exit 1
 			;;
 		esac
@@ -53,4 +68,8 @@ fi
 
 if [ "$DO_REMOVE_TEBIS" = true ]; then
 	remove_tebis
+fi
+
+if [ "$DO_REMOVE_YCSB" = true ]; then
+	remove_ycsb
 fi
