608832b17c6c6307f1900e98eba0ccd4701c0ac1
Dimokratis Stathakis <dstath@ics.forth.gr>
2024-07-24 17:54:51 +0300

    [feat] Script to run tebis in the pods

---
+++ dockers/exec_tebis.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+DEFAULT_ZKHOST="zk-cs:2181"
+DEFAULT_PORT=8080
+DEFAULT_THREADS=3
+
+usage() {
+	echo "Usage: $0 <partial-pod-name> <devname> <rdma> [-z <zkhost>] [-p <port>] [-t <threads>]"
+	exit 1
+}
+
+if [ $# -lt 3 ]; then
+	usage
+fi
+
+PARTIAL_POD_NAME=$1
+DEVNAME=$2
+RDMA=$3
+shift 3
+
+ZKHOST=$DEFAULT_ZKHOST
+PORT=$DEFAULT_PORT
+THREADS=$DEFAULT_THREADS
+
+# Parse optional arguments
+while getopts ":z:p:t:" opt; do
+	case ${opt} in
+	z)
+		ZKHOST=$OPTARG
+		;;
+	p)
+		PORT=$OPTARG
+		;;
+	t)
+		THREADS=$OPTARG
+		;;
+	\?)
+		usage
+		;;
+	esac
+done
+
+# Get the full pod name matching the partial pod name
+POD_NAME=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep "$PARTIAL_POD_NAME" | head -n 1)
+
+if [ -z "$POD_NAME" ]; then
+	echo "No pod found with name containing '$PARTIAL_POD_NAME'"
+	exit 1
+fi
+
+COMMAND="./build/tebis_server/tebis_server -d $DEVNAME -z $ZKHOST -r $RDMA -p $PORT -c $THREADS"
+
+echo "$COMMAND"
+kubectl exec -it "$POD_NAME" -- "$COMMAND"
