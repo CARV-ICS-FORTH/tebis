7d4788de84fbfced5c1d7ad865585a775b154ca8
Dimokratis Stathakis <dstath@ics.forth.gr>
2024-07-25 13:04:55 +0300

    [doc] Documentation for each function in server_config

--- tebis_server/server_config.h
+++ tebis_server/server_config.h
@@ -26,18 +26,66 @@ void SCONF_destroy_server_config(SCONF_server_config_t config);
 void SCONF_parse_arguments(int argc, char *argv[], SCONF_server_config_t config);
 
 /**
- * @brief Getter functions for server configuration fields.
+ * @brief Gets the device name from the server configuration.
  * @param config A pointer to the server configuration structure.
- * @return The corresponding field value from the server configuration structure.
+ * @return The device name.
  */
 char *SCONF_get_device_name(const SCONF_server_config_t config);
+
+/**
+ * @brief Gets the Zookeeper host from the server configuration.
+ * @param config A pointer to the server configuration structure.
+ * @return The Zookeeper host.
+ */
 char *SCONF_get_zk_host(const SCONF_server_config_t config);
+
+/**
+ * @brief Gets the RDMA subnet from the server configuration.
+ * @param config A pointer to the server configuration structure.
+ * @return The RDMA subnet.
+ */
 char *SCONF_get_rdma_subnet(const SCONF_server_config_t config);
+
+/**
+ * @brief Gets the TEBIS L0 size from the server configuration.
+ * @param config A pointer to the server configuration structure.
+ * @return The TEBIS L0 size.
+ */
 uint32_t SCONF_get_tebisl0_size(const SCONF_server_config_t config);
+
+/**
+ * @brief Gets the growth factor from the server configuration.
+ * @param config A pointer to the server configuration structure.
+ * @return The growth factor.
+ */
 uint32_t SCONF_get_growth_factor(const SCONF_server_config_t config);
+
+/**
+ * @brief Gets the index option (send or build) from the server configuration.
+ * @param config A pointer to the server configuration structure.
+ * @return 1 if send_index, 0 if build_index.
+ */
 int SCONF_get_index(const SCONF_server_config_t config);
+
+/**
+ * @brief Gets the server port from the server configuration.
+ * @param config A pointer to the server configuration structure.
+ * @return The server port.
+ */
 int SCONF_get_server_port(const SCONF_server_config_t config);
+
+/**
+ * @brief Gets the number of threads from the server configuration.
+ * @param config A pointer to the server configuration structure.
+ * @return The number of threads.
+ */
 int SCONF_get_num_threads(const SCONF_server_config_t config);
+
+/**
+ * @brief Gets the device size from the server configuration.
+ * @param config A pointer to the server configuration structure.
+ * @return The device size in GB.
+ */
 int SCONF_get_device_size(const SCONF_server_config_t config);
 
 #endif // SERVER_CONFIG_H
